17b288941956be8c8f544c1dedca04f6800a4c29
Jordan <jordan-huitema@live.nmit.ac.nz>
2020-10-29 10:33:07 +1300

    finished JS, JS missing to many bits, used alis JS, JS works fine now

--- src/script.js
+++ src/script.js
@@ -1,75 +1,162 @@
-// DOM selector
-const cardsCon = document.getElementById('cards-container')
-const prevBtn = document.getElementById('prev')
-const nextBtn = document.getElementById('next')
-const curEl = document.getElementById('current')
-const showEl = document.getElementById('show')
-const hideEl = document.getElementById('hide')
-const qustEl = document.getElementById('question')
-const ansEl = document.getElementById('answer')
-const addQuBtn = document.getElementById('add-question')
-const clrBtn = document.getElementById('clear')
-const addCon = document.getElementById('add-container')
+// DOM selector 
+const cardsContainer = document.getElementById('cards-container');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+const currentEl = document.getElementById('current');
+const showBtn = document.getElementById('show');
+const hideBtn = document.getElementById('hide');
+const questionEl = document.getElementById('question');
+const answerEl = document.getElementById('answer');
+const addQuestionBtn = document.getElementById('add-question');
+const clearBtn = document.getElementById('clear');
+const addContainer = document.getElementById('add-container');
 
-// Keep tracl of the current card
-let curActCd = 0 // show this card
+// Keep track of current card
+let currentActiveCard = 0; // Which Card to show
 
-// Store DOM cards array
-const cardsEl = []
+// Store DOM cards
+const cardsEl = []; //Store DOM cards which array of elements 
 
-// Store cards in local storage
-const cards = getCardsData()
+// Store card data
+const cardsData = getCardsData(); // fetch the data and store it in the local storage.
 
-// Create Cards
+// const cardsData = [
+//   {
+//     question: 'What must a variable begin with?',
+//     answer: 'A letter, $ or _'
+//   },
+//   {
+//     question: 'What is a variable?',
+//     answer: 'Container for a piece of data'
+//   },
+//   {
+//     question: 'Example of Case Sensitive Variable',
+//     answer: 'thisIsAVariable'
+//   }
+// ];
+
+// Create all cards
 function createCards() {
-    // loop through data and creating card array
-    cardsData.forEach((dat, idx) => createCards(dat, idx))
+  cardsData.forEach((data, index) => createCard(data, index)); // loop through the data and creat cards
 }
 
-// Create single card in DOM
-function createCard() {
-    const card = document.createElement('div')
+// Create a single card in DOM
+function createCard(data, index) { // Each Card will contain a question and answer
+  const card = document.createElement('div');
+  card.classList.add('card');
 
-    card.classList.add('card')
-    if (idx === 0) {
-        card.classList.add('active')
-    }
+  if (index === 0) {
+    card.classList.add('active');
+  }
 
-    card.innerHTML = `
-    <div class="inner-card">
-        <div class="inner-card-front">
-            <p>
-                ${data.question}
-            </p>
-        </div>
-        <div class="inner-card-back">
-            <p>
-                ${data.answer}
-            </p>
-        </div>
-    </div>`
+  card.innerHTML = `
+  <div class="inner-card">
+  <div class="inner-card-front">
+    <p>
+      ${data.question}
+    </p>
+  </div>
+  <div class="inner-card-back">
+    <p>
+      ${data.answer}
+    </p>
+  </div>
+</div>
+  `;
 
-    card.addEventListener('click', () => card.classList.toggle('show-answer'))
+  card.addEventListener('click', () => card.classList.toggle('show-answer'));
 
-    // Add to DOM cards
-    cardsEl.push(card)
-    cardsContainer.appendChild(card)
-    updateCurrentText()
+  // Add to DOM cards
+  cardsEl.push(card);
+
+  cardsContainer.appendChild(card); // Put in the container
+
+  updateCurrentText();
 }
 
 // Show number of cards
 function updateCurrentText() {
-    curEl.innerText = `${curActCd + 1}/${cardsEl.length}` 
+  currentEl.innerText = `${currentActiveCard + 1}/${cardsEl.length}`; // Add 1 to it because it's 0 by default 
 }
 
 // Get cards from local storage
-function getCardsData() {
-    const cards = JSON.parse(localStorage.getItem('cards'))
-    return cards === null ? [] : cards
+function getCardsData() { 
+  // local storage only store string so we will take the array fetch back as an array by using parse
+  const cards = JSON.parse(localStorage.getItem('cards')); 
+  return cards === null ? [] : cards; //  if the cards are null return an empty array 
 }
 
 // Add card to local storage
 function setCardsData(cards) {
-    // We want to turn it to a JSON string
-    localStorage.setItem('cards', JSON.stringify(cards))
+  // Adding a card to the array and overwrite the array in storage
+  localStorage.setItem('cards', JSON.stringify(cards)); // We want to turn it to a sting
+  window.location.reload(); // To reflect on the DOM
 }
+
+createCards();
+
+// Event listeners
+
+// Next button
+nextBtn.addEventListener('click', () => {
+  cardsEl[currentActiveCard].className = 'card left';
+
+  currentActiveCard = currentActiveCard + 1; // If we are at 1 it will 2 and so on
+
+  // We need to set the index on the last card.
+  if (currentActiveCard > cardsEl.length - 1) {  // -1 is because the array base is 0 
+    currentActiveCard = cardsEl.length - 1; // Set current active card to last place
+  }
+
+  cardsEl[currentActiveCard].className = 'card active'; // Set the next card active by overwriting class name and gives the effect in CSS
+
+  updateCurrentText(); // To update the card numbers
+});
+
+// Prev button
+prevBtn.addEventListener('click', () => {
+  cardsEl[currentActiveCard].className = 'card right';
+
+  currentActiveCard = currentActiveCard - 1; // -1 for going back 
+
+  if (currentActiveCard < 0) { // To prevent going pass 0 
+    currentActiveCard = 0; // Set the current to 0 
+  }
+
+  cardsEl[currentActiveCard].className = 'card active'; // Set the next card active by overwriting class name and gives the effect in CSS
+
+  updateCurrentText();
+});
+
+// Show add container
+showBtn.addEventListener('click', () => addContainer.classList.add('show')); // Add show class which will present our form card with CSS style
+// Hide add container
+hideBtn.addEventListener('click', () => addContainer.classList.remove('show')); // Add the hide container CSS style.
+
+// Add new card
+addQuestionBtn.addEventListener('click', () => {
+  const question = questionEl.value; // Getting values from the form
+  const answer = answerEl.value; // Getting values from the form
+  console.log(question, answer)
+
+  if (question.trim() && answer.trim()) {
+    const newCard = { question, answer }; // Create a new card object
+
+    createCard(newCard);
+
+    questionEl.value = ''; // Clear the form inputs
+    answerEl.value = ''; // Clear the form inputs
+
+    addContainer.classList.remove('show'); // Hide class container
+
+    cardsData.push(newCard); // Add new card to the array
+    setCardsData(cardsData); // Passing to storage
+  }
+});
+
+// Clear cards button
+clearBtn.addEventListener('click', () => {
+  localStorage.clear(); // using clear method
+  cardsContainer.innerHTML = ''; // take the cards out of the DOM 
+  window.location.reload(); // Then reloading to update the page
+});
